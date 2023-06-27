6124aee383a2bd16bdac4f08d5f5a42097e75811
RashellChandler <rashell.chandler@yahoo.com>
2023-06-26 21:29:18 -0500

    Variables Exercise

---
+++ Variables2/Program.cs
@@ -0,0 +1,14 @@
+﻿string name = "Shellie";
+int age = 41;
+char middleInitial = 'C';
+bool isOver18 = true;
+double currentTemp = 80;
+decimal currentPrice = 29.99m;
+
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
